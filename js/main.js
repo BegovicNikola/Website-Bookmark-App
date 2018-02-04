82747565d88201c0a0870ccb12479f8589bc5319
BegovicNikola <begovic.office@gmail.com>
2018-02-04 02:22:09 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
+
+function saveBookmark(e) {
+	// Get form values
+	
+	// Prevent from submitting
+	e.preventDefault();
+}
